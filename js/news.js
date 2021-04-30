38768e2f5aeee8cc230a2c7f88bdb22f0205199c
Edhyjox <edhyjox@gmail.com>
2021-04-30 22:24:52 +0200

    Implementation of Local Storage

--- js/news.js
+++ js/news.js
@@ -1,42 +1,60 @@
+let news = []
+
+
 class News {
     constructor(parent, title, description) {
-        // if (!title.length || !description.length) return
+        if (!title.length || !description.length) return  // Dont want empty news !
 
         this.title = title
         this.description = description
-        this.parent = document.getElementById(parent)
+        this.parent = parent
 
+        this.create()
+    }
+
+    create() {
+        // Building tags
         let article = document.createElement('article')
         let headerArticle = document.createElement('header')
         let imgBin = document.createElement('img')
         let h3 = document.createElement('h3')
         let p = document.createElement('p')
 
+
+        // Trash Can
         imgBin.src = "svg/trash.svg"
         imgBin.style.height = "2em"
         imgBin.style.width = "auto"
 
+        // Content & Edition
         h3.textContent = this.title
         p.textContent = this.description
-
         h3.contentEditable = "true"
         p.contentEditable = "true"
 
-        imgBin.addEventListener("click", (e) => {this.delete(e)})
+        imgBin.addEventListener(
+            "click",
+            (e) => {
+                this.delete(e)
+            }
+        )
 
         headerArticle.appendChild(h3)
         headerArticle.appendChild(imgBin)
         article.appendChild(headerArticle)
         article.appendChild(p)
 
-        this.parent.appendChild(article)
+        // avoid {} in json stringification
+        document.getElementById(this.parent).appendChild(article)
     }
+
 
     delete(e) {
         e.currentTarget.style.display = "none" // remove the trash can
-        let article = e.currentTarget.parentNode.parentNode
-        article.style.borderTopColor = "#CD5C73"
+        let article = e.currentTarget.parentNode.parentNode // Oh no structure !
+        article.style.borderTopColor = "#CD5C73" // Because it's kinda cool
 
+        // Thanos snap
         article.animate(
             [
                 {opacity: 1},
@@ -50,29 +68,44 @@ class News {
 }
 
 function addNews() {
+    // add This !
     new News(
         document.getElementById("titre").value,
         document.getElementById("description").value,
         "news_wrapper"
     )
+    Save()
+}
+
+
+function addDefaultNews() {
+    news.push(
+        new News(
+            "news_wrapper",
+            "Note de l'auteur !",
+            "Ce site est est démo exemple, Design & Concept : Yohann Boniface"
+        )
+    )
+
+    Save()
+}
+
+
+function Save() {
+    localStorage.setItem("news", JSON.stringify(news))
+}
+
+
+function loadNews() {
+    let data = JSON.parse(localStorage.getItem("news"))
+    data.forEach(
+        (news) => {
+            new News(news.parent, news.title, news.description)
+        }
+    )
 }
 
 
 window.onload = () => {
-    new News(
-        "news_wrapper",
-        "Créez votre Article !",
-        "Créez maintenant votre article grâce au menu juste au dessus ! Supprimez cet article en appuyant sur l'icône poubelle !",
-    )
-
-    setTimeout(
-        () => {
-            new News(
-                "news_wrapper",
-                "Note de l'auteur !",
-                "Ce site est est démo exemple, Design & Concept : Yohann Boniface"
-
-            )
-        }, 500
-    )
+    (localStorage.getItem("infiniteScrollEnabled") === null) ? addDefaultNews(): loadNews()
 }
